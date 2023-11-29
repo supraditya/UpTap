25742d24ed488d27d8891ce48c011ec31164c667
supraditya <aditya300100@gmail.com>
2023-11-29 18:02:40 -0500

    Qr code scanning basics complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,74 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchData } from "../app/profileSlice";
-import QRCode from "react-native-qrcode-svg";
+  import React, { useState, useEffect } from "react";
+  import { StyleSheet, Text, View, Button } from "react-native";
+  import { useDispatch, useSelector } from "react-redux";
+  import { fetchData } from "../app/profileSlice";
 
-const HomeScreen = () => {
-  const dispatch = useDispatch();
-  const { data, status, error } = useSelector((state) => state.profile);
+  import { BarCodeScanner } from "expo-barcode-scanner";
+  import QRCode from "react-native-qrcode-svg";
 
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
+  const HomeScreen = () => {
+    // Redux content
+    const dispatch = useDispatch();
+    const { data, status, error } = useSelector((state) => state.profile);
 
-  return (
-    <View>
-      <View style={{ alignItems: 'center'}}>{data.qr_link && <QRCode value={data.qr_link} size={150} />}</View>
-      <Text style={{ fontSize: 30 }}>
-        {data.firstName} {data.lastName}
-      </Text>
-    </View>
-  );
-};
+    useEffect(() => {
+      dispatch(fetchData());
+    }, [dispatch]);
 
-export default HomeScreen;
+    // Expo barcode scanner content (Permission and scanning)
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(true);
+
+    useEffect(() => {
+      const getBarCodeScannerPermissions = async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === "granted");
+      };
+
+      getBarCodeScannerPermissions();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+      setScanned(true);
+      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    return (
+      <View>
+        <View style={{ alignItems: "center" }}>
+          {data.qr_link && <QRCode value={data.qr_link} size={150} />}
+        </View>
+        <Text style={{ fontSize: 30 }}>
+          {data.firstName} {data.lastName}
+        </Text>
+        <View>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={styles.cameraContainer}
+          />
+          {scanned && (
+            <Button
+              title={"Tap to Scan Again"}
+              onPress={() => setScanned(false)}
+            />
+          )}
+        </View>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    cameraContainer: {
+      height: 250, // Adjust the height as needed
+      width: 250, // Take the full width of the parent container
+    },
+  });
+
+  export default HomeScreen;
