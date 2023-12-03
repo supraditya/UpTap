f2e32695747315b2f8cee6f51d3f7dd843054f45
supraditya <aditya300100@gmail.com>
2023-12-03 11:53:04 -0500

    Formatted files

--- app/authManager.js
+++ app/authManager.js
@@ -1,57 +1,58 @@
 import {
-    getAuth, signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    updateProfile, signOut as fbSignOut,
-    initializeAuth,
-    getReactNativePersistence,
-    onAuthStateChanged
-} from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import app from './firebase'
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signOut as fbSignOut,
+  initializeAuth,
+  getReactNativePersistence,
+  onAuthStateChanged,
+} from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import app from "./firebase";
 
 try {
-    auth = initializeAuth(app, {
-        persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-    });
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
 } catch (error) {
-    auth = getAuth(app); // if auth already initialized
+  auth = getAuth(app); // if auth already initialized
 }
 
 const signIn = async (email, password) => {
-    await signInWithEmailAndPassword(auth, email, password);
-}
+  await signInWithEmailAndPassword(auth, email, password);
+};
 
 const signOut = async () => {
-    await fbSignOut(auth);
-}
+  await fbSignOut(auth);
+};
 
 const getAuthUser = () => {
-    return auth.currentUser;
-}
+  return auth.currentUser;
+};
 
 const signUp = async (displayName, email, password) => {
-    const userCred = await createUserWithEmailAndPassword(auth, email, password);
-    await updateProfile(userCred.user, { displayName: displayName });
-    return userCred.user;
-}
+  const userCred = await createUserWithEmailAndPassword(auth, email, password);
+  await updateProfile(userCred.user, { displayName: displayName });
+  return userCred.user;
+};
 
 let unsubscribeFromAuthChanges = undefined;
 
 const subscribeToAuthChanges = (navigation) => {
+  if (unsubscribeFromAuthChanges) {
+    unsubscribeFromAuthChanges();
+  }
 
-    if (unsubscribeFromAuthChanges) {
-        unsubscribeFromAuthChanges();
+  unsubscribeFromAuthChanges = onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User sign in successful
+      navigation.navigate("Home");
+    } else {
+      // User sign out successful
+      navigation.navigate("Login");
     }
+  });
+};
 
-    unsubscribeFromAuthChanges = onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User sign in successful
-            navigation.navigate('Home');
-        } else {
-            // User sign out successful
-            navigation.navigate('Login');
-        }
-    })
-}
-
-export { signIn, signOut, signUp, getAuthUser, subscribeToAuthChanges }
+export { signIn, signOut, signUp, getAuthUser, subscribeToAuthChanges };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,18 +3,17 @@ import { StyleSheet, Text, View, Alert, FlatList } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../app/profileSlice";
 
-import { Button } from '@rneui/themed';
-import { getAuth } from 'firebase/auth';
-import { getApps, initializeApp } from 'firebase/app';
-import { firebaseConfig } from '../Secrets';
-import { getAuthUser, signOut } from '../app/authManager';
-import { subscribeToUserUpdates } from '../data/Actions';
-
+import { Button } from "@rneui/themed";
+import { getAuth } from "firebase/auth";
+import { getApps, initializeApp } from "firebase/app";
+import { firebaseConfig } from "../Secrets";
+import { getAuthUser, signOut } from "../app/authManager";
+import { subscribeToUserUpdates } from "../data/Actions";
 
 function HomeScreen({ navigation }) {
   const dispatch = useDispatch();
   const { data, status, error } = useSelector((state) => state.profile);
-  const users = useSelector(state => state.users);
+  const users = useSelector((state) => state.users);
   const currentAuthUser = getAuthUser();
 
   useEffect(() => {
@@ -23,17 +22,15 @@ function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text>
-        You're signed in, {currentAuthUser && currentAuthUser.email}!
-      </Text>
+      <Text>You're signed in, {currentAuthUser && currentAuthUser.email}!</Text>
 
       <Button
         onPress={async () => {
           try {
             await signOut();
-            navigation.navigate('Login');
+            navigation.navigate("Login");
           } catch (error) {
-            Alert.alert("Sign In Error", error.message, [{ text: "OK" }])
+            Alert.alert("Sign In Error", error.message, [{ text: "OK" }]);
           }
         }}
       >
@@ -41,20 +38,20 @@ function HomeScreen({ navigation }) {
       </Button>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'pink'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "pink",
   },
 
   listContainer: {
     flex: 0.5,
-    witdh: '100%',
-  }
+    witdh: "100%",
+  },
 });
 
 export default HomeScreen;
