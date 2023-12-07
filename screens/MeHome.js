e78d7c837a15652a5ad96e5e978911950d4b9a39
supraditya <aditya300100@gmail.com>
2023-12-07 12:25:10 -0500

    Styled my cards

--- screens/MeHome.js
+++ screens/MeHome.js
@@ -1,12 +1,17 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from "react-native";
-
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
 
 import { useDispatch, useSelector } from "react-redux";
 import QRCode from "react-native-qrcode-svg";
 
 import { getAuthUser } from "../app/authManager";
 import { colorCalculate } from "./PeopleHome";
-
 
 const MeHome = ({ navigation }) => {
   const currentAuthUser = getAuthUser();
@@ -16,44 +21,30 @@ const MeHome = ({ navigation }) => {
   );
 
   const meCardPressHandler = (card) => {
-    navigation.navigate("MyCardScreen", { card: card })
-  }
-
+    navigation.navigate("MyCardScreen", { card: card });
+  };
 
   return (
     <View style={styles.container}>
-      <View style={styles.myCard}>
-        {currentAuthUser &&
-          userData.my_cards_data_list.map((card) => {
-            return (
-              <View>
-                <TouchableOpacity onPress={() => meCardPressHandler(card)} key={card.id}>
-                  <View>
-                    {/* <Text style={{ fontSize: 32 }}>{card.nameOfCard}</Text> */}
-                    <Text style={styles.cardTextHeaderStyle}>
-                      {card.firstName} {card.lastName}
-                    </Text>
-                    <Text style={styles.cardTextStyle}>Works at {card.company}</Text>
-                    {Object.entries(card).map(([keyName, value], index) => {
-                      if (
-                        keyName !== "firstName" &&
-                        keyName !== "lastName" &&
-                        keyName !== "id" &&
-                        keyName !== "company"
-                      ) {
-                        return (
-                          <Text style={styles.cardTextStyle} key={index}>
-                            {keyName}: {value}
-                          </Text>
-                        );
-                      }
-                    })}
-                  </View>
-                </TouchableOpacity>
-              </View>
-            )
-          })}
-      </View>
+      {currentAuthUser &&
+        userData.my_cards_data_list.map((card) => {
+          return (
+            <View style={{...styles.myCard, backgroundColor: colorCalculate()}}>
+              <TouchableOpacity
+                onPress={() => meCardPressHandler(card)}
+                key={card.id}
+              >
+                <View>
+                  {/* <Text style={{ fontSize: 32 }}>{card.nameOfCard}</Text> */}
+                  <Text style={styles.cardTextHeaderStyle}>
+                    My {card.company} Card
+                  </Text>
+                  {/* <Text style={styles.cardTextStyle}>Works at {card.company}</Text> */}
+                </View>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
     </View>
   );
 };
@@ -75,19 +66,20 @@ const styles = StyleSheet.create({
     color: "white",
   },
   myCard: {
-    backgroundColor: colorCalculate(),
-    flex: 0.5,
+    // backgroundColor: colorCalculate(),
+    flex: 0.2,
     borderRadius: 18,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '90%'
+    marginVertical: '5%',
+    justifyContent: "center",
+    alignItems: "center",
+    width: "90%",
   },
   cardTextStyle: {
     fontSize: 20,
   },
   cardTextHeaderStyle: {
     fontSize: 36,
-  }
+  },
 });
 
 export default MeHome;
--- screens/MyCardScreen.js
+++ screens/MyCardScreen.js
@@ -2,31 +2,51 @@ import { View, Text, StyleSheet, Alert, TouchableOpacity } from "react-native";
 
 import { useSelector } from "react-redux";
 import QRCode from "react-native-qrcode-svg";
-
+import { colorCalculate } from "./PeopleHome";
 
 const MyCardScreen = ({ route, navigation }) => {
   const card = route.params.card;
   // console.log(card)
   return (
     <View style={styles.container}>
-      <QRCode value={card.id} size={150} />
-      <Text>{card.nameOfCard}</Text>
-      <Text>{card.firstName}</Text>
       <TouchableOpacity
-        onPress={() => navigation.navigate('CardEditScreen',
-          {
-            card: card
-          }
-        )}
+        onPress={() => {
+          navigation.goBack();
+        }}
+        style={styles.backButton}
       >
-        <Text>
-          Edit
-        </Text>
+        <Text style={styles.backButtonText}>Back</Text>
       </TouchableOpacity>
 
+      <View style={styles.cardContainer}>
+        <QRCode value={card.id} size={150} />
+        {Object.entries(card).map(([keyName, value], index) => {
+          if (
+            keyName !== "firstName" &&
+            keyName !== "lastName" &&
+            keyName !== "id" &&
+            keyName !== "company"
+          ) {
+            return (
+              <Text style={styles.cardTextStyle} key={index}>
+                {keyName}: {value}
+              </Text>
+            );
+          }
+        })}
+      </View>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("CardEditScreen", {
+            card: card,
+          })
+        }
+      >
+        <Text style={styles.editButtonText}>Edit</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -48,15 +68,36 @@ const styles = StyleSheet.create({
     backgroundColor: colorCalculate(),
     flex: 0.5,
     borderRadius: 18,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '90%'
+    justifyContent: "center",
+    alignItems: "center",
+    width: "90%",
   },
   cardTextStyle: {
+    marginVertical: 5,
     fontSize: 20,
   },
   cardTextHeaderStyle: {
     fontSize: 36,
+  },
+  cardContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colorCalculate(),
+    padding: "5%",
+    height: "60%",
+    width: "90%",
+    borderRadius: 18,
+  },
+  editButtonText: {
+    fontSize: 28,
+    marginTop: 15,
+  },
+  backButton:{
+    width: '85%',
+    // fontSize: 24,
+  },
+  backButtonText:{
+    fontSize: 24,
   }
 });
 
